7988cc1a565b3320ad53c57bd6d95a25c92678eb
Lawrence <lawrence.peters@gmail.com>
2017-08-24 14:52:04 +0100

    Mostly finished

--- main.js
+++ main.js
@@ -1,11 +1,53 @@
 $(document).ready(function()
 {
-  var content = "<table>"
-  for(i = 0 ; i < 256; i++)
+  $('#enterButton').click(function()
   {
-      content += '<div class=box></div>';
-  }
-  content += "</table>"
+    var content = "<table>";
+    var gridWidth = document.getElementById('gridWidth').value;
+    var gridHeight = document.getElementById('gridHeight').value;
+    var gridSize = gridHeight*gridWidth;
+    var heightFraction = "";
+    var widthFraction = "";
 
-  $('#mainGrid').append(content);
+    for(var i = 0; i < gridHeight; i++)
+    {
+      heightFraction += " 1fr";
+    }
+
+    for(var i = 0; i < gridWidth; i++)
+    {
+      widthFraction += " 1fr";
+    }
+
+    $('.wrapper').css({'grid-template-rows': widthFraction});
+    $('.wrapper').css({'grid-template-columns': heightFraction});
+
+    for(var i = 0 ; i < gridSize; i++)
+    {
+        content += '<div class=box></div>';
+    }
+
+    content += "</table>";
+    $('.wrapper').append(content);
+
+    $('.box').hover(function()
+      {
+        $(this).css({'background-color': function getRandomColor()
+        {
+          var letters = '0123456789ABCDEF';
+          var color = '#';
+          for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      }});
+      });
+
+    $(this).hide();
+  });
+
+  $('#clearButton').click(function()
+  {
+    $('.box.hidden').removeClass('hidden');
+  });
 });
